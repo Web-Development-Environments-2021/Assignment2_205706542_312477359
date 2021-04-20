ee66abaab6ee778109fa8d9626b299eba940ba8a
nir-st <nirstein@post.bgu.ac.il>
2021-04-20 12:38:11 +0300

    added about dialog

--- javascript/menu.js
+++ javascript/menu.js
@@ -9,3 +9,22 @@ function set_active_menu_item(item_name) {
     current_active_menu_item = item_name;
     $('#' + current_active_menu_item + '-div').show();
 }
+
+// about modal
+var about_modal = document.getElementById("about-modal-id");
+var span = document.getElementById("about-close-btn");
+
+function open_about_modal() {
+    about_modal.style.display = "block";
+}
+
+span.onclick = function() {
+    about_modal.style.display = "none";
+}
+  
+
+window.onclick = function(event) {
+  if (event.target == about_modal) {
+    about_modal.style.display = "none";
+  }
+}
