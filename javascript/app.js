cef9fdb3419992e9fed9a0ac1789b442d06f36e4
nir-st <nirstein@post.bgu.ac.il>
2021-04-26 12:24:39 +0300

    almost ghosts

--- javascript/app.js
+++ javascript/app.js
@@ -8,10 +8,13 @@ var time_elapsed;
 var interval;
 var context = canvas.getContext('2d');
 
-cell_height = 60;
-cell_width = 60;
+var cell_height = 60;
+var cell_width = 60;
 
+// music
 var game_music = new Audio('media/game_music.mp3');
+
+// bonus
 var bonus = new Object();
 bonus.image = new Image(cell_width, cell_height);
 bonus.image.src = './media/hamburger.jpg';
@@ -28,6 +31,29 @@ var food_remain;
 var food_low_color;
 var food_mid_color;
 var food_high_color;
+
+// ghots
+var monster_count;
+
+var pinky = new Object();
+pinky.image = new Image(cell_width-4, cell_height-4);
+pinky.image.src = './media/Pinky.jpeg';
+pinky.id = 8;
+
+var inky = new Object();
+inky.image = new Image(cell_width-4, cell_height-4);
+inky.image.src = './media/Inky.jpeg';
+inky.id = 9;
+
+var clyde = new Object();
+clyde.image = new Image(cell_width-4, cell_height-4);
+clyde.image.src = './media/Clyde.jpeg';
+clyde.id = 10;
+
+var blinky = new Object();
+blinky.image = new Image(cell_width-4, cell_height-4);
+blinky.image.src = './media/Blinky.jpeg';
+blinky.id = 11;
 
 var game_time;
 
@@ -52,7 +78,29 @@ function Start() {
 		board[i] = new Array();
 
 		for (var j = 0; j < 10; j++) {
-			if (
+			//ghots	
+			if (i == 0 && j == 0 && blinky.show) {
+				board[i][j] = blinky.id
+				blinky.i = 0;
+				blinky.j = 0;
+			}
+			else if (i == 0 && j == 9 && clyde.show) {
+				board[i][j] = clyde.id
+				clyde.i = 0;
+				clyde.j = 0;
+			}
+			else if (i == 9 && j == 0 && inky.show) {
+				board[i][j] = inky.id
+				inky.i = 0;
+				inky.j = 0;
+			}
+			else if (i == 0 && j == 0 && pinky.show) {
+				board[i][j] = pinky.id
+				pinky.i = 0;
+				pinky.j = 0;
+			}
+			//walls
+			else if (
 				(i == 3 && j == 3) ||
 				(i == 3 && j == 4) ||
 				(i == 3 && j == 5) ||
@@ -60,7 +108,8 @@ function Start() {
 				(i == 6 && j == 2)
 			) {
 				board[i][j] = 4;
-			} else {
+			} 
+			else {
 				var randomNum = Math.random();
 				if (randomNum <= (0.1 * food_remain) / cnt) {
 					food_remain--;
@@ -70,12 +119,12 @@ function Start() {
 					food_remain--;
 					board[i][j] = 6;
 				}
-				else if (randomNum <= (1.0 * food_remain) / cnt) {
+				else if (randomNum <= (0.6 * food_remain) / cnt) {
 					food_remain--;
 					board[i][j] = 1;
 				}
 			
-				else if (randomNum < (1.0 * (pacman_remain + food_remain)) / cnt) {
+				else if (pacman_remain > 0) {
 					shape.i = i;
 					shape.j = j;
 					pacman_remain--;
@@ -128,6 +177,43 @@ function configureGameSettings() {
 	food_high_color = document.getElementById('food-color-high').value;
 
 	game_time = document.getElementById('game-time-input-id').value;
+
+	monster_count = document.getElementById('monster-count-id').value;
+
+	if (monster_count == 1) {
+		pinky.show = false;
+		inky.show = false;
+		clyde.show = false;
+		blinky.show = true;
+		blinky.also = 0;
+	}
+	if (monster_count == 2) {
+		pinky.show = false;
+		inky.show = false;
+		clyde.show = true;
+		blinky.show = true;
+		clyde.also = 0;
+		blinky.also = 0;
+	}
+	if (monster_count == 3) {
+		pinky.show = false;
+		inky.show = true;
+		clyde.show = true;
+		blinky.show = true;
+		inky.also = 0;
+		clyde.also = 0;
+		blinky.also = 0;
+	}
+	if (monster_count == 3) {
+		pinky.show = true;
+		inky.show = true;
+		clyde.show = true;
+		blinky.show = true;
+		pinky.also = 0;
+		inky.also = 0;
+		clyde.also = 0;
+		blinky.also = 0;
+	}
 }
 
 function findRandomEmptyCell(board) {
@@ -293,6 +379,21 @@ function Draw(x) {
 			else if (board[i][j] == 5 && bonus.show) {  // bonus
 				context.drawImage(bonus.image, center.x - (cell_width / 2) + 2, center.y - (cell_height / 2) + 2, cell_width - 4, cell_height - 4);
 			}
+
+			else if (board[i][j] == blinky.id && blinky.show) {  // blinky
+				context.drawImage(blinky.image, center.x - (cell_width / 2) + 2, center.y - (cell_height / 2) + 2, cell_width - 4, cell_height - 4);
+			}
+
+			else if (board[i][j] == clyde.id && clyde.show) {  // clyde
+				context.drawImage(clyde.image, center.x - (cell_width / 2) + 2, center.y - (cell_height / 2) + 2, cell_width - 4, cell_height - 4);
+			}
+			else if (board[i][j] == inky.id && inky.show) {  // inky
+				context.drawImage(inky.image, center.x - (cell_width / 2) + 2, center.y - (cell_height / 2) + 2, cell_width - 4, cell_height - 4);
+			}
+			// put pinky
+			else if (board[i][j] == pinky.id && pinky.show) {  // pinky
+				context.drawImage(pinky.image, center.x - (cell_width / 2) + 2, center.y - (cell_height / 2) + 2, cell_width - 4, cell_height - 4);
+			}
 		}
 	}
 }
@@ -321,7 +422,7 @@ function UpdatePosition() {
 			shape.i = 9;
 		}
 	}
-	if (x == 4) {  // righgt
+	if (x == 4) {  // right
 		if (shape.i < 9 && board[shape.i + 1][shape.j] != 4) {
 			shape.i++;
 		}
@@ -332,6 +433,20 @@ function UpdatePosition() {
 
 	if (bonus.show == true) {
 		move_bonus();
+	}
+
+	// move ghots
+	if(blinky.show) {
+		move_ghost(blinky);
+	}
+	if(clyde.show) {
+		move_ghost(clyde);
+	}
+	if(inky.show) {
+		move_ghost(inky);
+	}
+	if(pinky.show) {
+		move_ghost(pinky);
 	}
 
 	if (board[shape.i][shape.j] == 1) {  // food-low
@@ -429,7 +544,9 @@ function move_bonus() {
 		bonus.x += 1;
 	}
 
-	bonus.also = board[bonus.x][bonus.y];
+	if (board[bonus.x][bonus.y] != 2) {
+		bonus.also = board[bonus.x][bonus.y];
+	}
 	board[bonus.x][bonus.y] = 5;
 
 }
@@ -444,4 +561,64 @@ function updateLives(num) {
 		document.getElementById('heart' + i.toString()).style.visibility = 'hidden';
 		i++;
 	}
+}
+
+// ** GHOSTS LOGIC ** //
+function calc_available_ghost_moves(ghost) {
+	// return array of avaliable moves for the ghost
+	// 0-up , 1-down , 2-left , 3-right
+	let available_directions = []
+
+	if (ghost.j > 0 && board[ghost.i][ghost.j - 1] != 4) {  // up
+		available_directions.push(0)
+	}
+	if (ghost.j < 9 && board[ghost.i][ghost.j + 1] != 4) {  // down
+		available_directions.push(1)
+	}
+	if (ghost.i > 0 && board[ghost.i - 1][ghost.j] != 4) {  // left
+		available_directions.push(2)
+	}
+	if (ghost.i < 9 && board[ghost.i + 1][ghost.j] != 4) {  // right
+		available_directions.push(3)
+	}
+	return available_directions
+}
+
+
+function move_ghost(ghost) {
+
+	let available_directions = calc_available_ghost_moves(ghost);
+	
+	if (ghost.also != 2) {
+		board[ghost.i][ghost.j] = ghost.also;  // put back what was there before
+	}
+
+	let dir = available_directions[Math.floor(Math.random() * available_directions.length)];
+
+	// update ghost coordinates
+	if (dir == 0) {  // up
+		ghost.j -= 1;
+	}
+	else if (dir == 1) {  // down
+		ghost.i += 1;
+	}
+	else if (dir == 2) {  // left
+		ghost.j -= 1;
+	}
+	else if (dir == 3) {  // right
+		ghost.i += 1;
+	}
+
+	// remember what is there
+	if (
+		(board[ghost.i][ghost.j] != 2) && 
+		(board[ghost.i][ghost.j] != 5) && 
+		(board[ghost.i][ghost.j] != 8) && 
+		(board[ghost.i][ghost.j] != 9) && 
+		(board[ghost.i][ghost.j] != 10) && 
+		(board[ghost.i][ghost.j] != 11)
+	){
+		ghost.also = board[ghost.i][ghost.j];
+	}
+	board[ghost.i][ghost.j] = ghost.id;
 }
