be4781bf28d7f106e1135a2f5ba258ba544fd478
omermarc <58112829+omermarc@users.noreply.github.com>
2021-04-27 22:29:57 +0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,179 @@
+var scores = []
+var keys=[]
+
+var current_active_menu_item = "welcome"
+
+function hide_active_menu_item() {
+    if (current_active_menu_item == 'game') {
+        game_music.pause()
+        game_music.currentTime = 0;
+        window.clearInterval(interval);
+        window.clearInterval(ghostsInterval);
+        window.clearInterval(bonusInterval);
+    }
+    $('#' + current_active_menu_item + '-div').hide();
+}
+
+function set_active_menu_item(item_name) {
+    hide_active_menu_item();
+    current_active_menu_item = item_name;
+    if (item_name == 'scores') {
+        updateScores();
+    }
+    $('#' + current_active_menu_item + '-div').show();
+}
+
+// about modal
+var about_modal = document.getElementById("about-modal-id");
+var span = document.getElementById("about-close-btn");
+
+function open_about_modal() {
+    about_modal.style.display = "block";
+}
+
+span.onclick = function() {
+    about_modal.style.display = "none";
+}
+  
+
+window.onclick = function(event) {
+  if (event.target == about_modal) {
+    about_modal.style.display = "none";
+  }
+}
+
+$(document).keydown(function(event) { 
+    if (event.keyCode == 27) { 
+        about_modal.style.display = "none";
+    }
+});
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+function randomizeSettings() {
+
+    document.getElementById('upKey').value = 'Arrow Up';
+    document.getElementById('downKey').value = 'Arrow Down';
+    document.getElementById('leftKey').value = 'Arrow Left';
+    document.getElementById('rightKey').value = 'Arrow Right';
+
+    let randomFoodCount = Math.floor(Math.random() * (90 - 50) ) + 50;
+    document.getElementById('food-count').value = randomFoodCount;
+    updateSliderValue()
+    
+    document.getElementById('food-color-low').value = getRandomColor();
+    document.getElementById('food-color-mid').value = getRandomColor();
+    document.getElementById('food-color-high').value = getRandomColor();
+
+    let randomGameTime = Math.floor(Math.random() * (180 - 60) ) + 60;
+    document.getElementById('game-time-input-id').value = randomGameTime
+
+    selectRandom()  // select a random monster count
+}
+
+function selectRandom() {
+    // selecting a random option form the monster count options in settings
+    var select = document.getElementById('monster-count-id');
+    var items = select.getElementsByTagName('option');
+    var index = Math.floor(Math.random() * items.length);
+
+    select.selectedIndex = index;
+}
+
+function getKeyName(keyCode)
+{
+    if(keyCode == 37) {
+		return "Arrow Left";
+	}
+	else if(keyCode == 38){
+		return "Arrow Up";
+	}
+	else if(keyCode == 39) {
+		return "Arrow Right";
+	}
+    else if(keyCode == 40) {
+		return "Arrow Down";
+	}
+	else {
+		return String.fromCharCode(keyCode);
+	}
+}
+
+function resetKeyButtonsColors() {
+
+    document.getElementById('upKey').style.backgroundColor = 'white';
+    document.getElementById('downKey').style.backgroundColor = 'white';
+    document.getElementById('rightKey').style.backgroundColor = 'white';
+    document.getElementById('leftKey').style.backgroundColor = 'white';
+}
+
+function highlightKeyButton(direction) {
+
+    resetKeyButtonsColors()
+    document.getElementById(direction + 'Key').style.backgroundColor = 'yellow';
+}
+
+function setKey(event, direction) {
+	
+    resetKeyButtonsColors()
+    document.getElementById(direction + 'Key').style.backgroundColor = 'yellow';
+    let keyName;
+	let keyCode;
+    keyCode = event.keyCode;
+    keyName = getKeyName(keyCode)
+    document.getElementById(direction + 'Key').value = keyName;
+    
+    
+    
+    if (direction == 'up') {
+        key_up = event.keyCode;
+        lblChosenUp.value = keyName; 
+    }
+    else if (direction == 'down') {
+        key_down = event.keyCode;
+        lblChosenDown.value = keyName;  
+    }
+    else if (direction == 'left') {
+        key_left = event.keyCode;
+        lblChosenLeft.value = keyName;   
+    }
+    else if (direction == 'right') {
+        key_right = event.keyCode;
+        lblChosenRight.value = keyName;
+    }
+}
+
+
+
+function updateSliderValue() {
+    var slider = document.getElementById("food-count");
+    var output = document.getElementById("display-food-count");
+    
+    output.innerHTML = slider.value;
+    lblFood.value= slider;
+}
+
+// validate game settings
+$("#settings-form").validate({
+    submitHandler: function(form) {
+        set_active_menu_item('game')
+        Start();
+    }
+});
+
+function updateScores() {
+    // sort the scores array
+    scores.sort(function (first, second) { return (first[1] - second[1]); } );
+
+    for (var i=0; i < scores.length; i++) {
+        document.getElementById('score-table-username-' + i.toString()).innerHTML = scores[i][0];
+        document.getElementById('score-table-time-' + i.toString()).innerHTML = scores[i][1];
+    }
+}
